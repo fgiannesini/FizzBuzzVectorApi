e6d7c268749e270876b85ddb256aba2c6a09676a
Fabien Giannesini <fabien.giannesini@gmail.com>
2024-06-01 15:32:45 +0200

    Upgrade libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,15 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_22
+    targetCompatibility = JavaVersion.VERSION_22
 }
 
 dependencies {
     testImplementation("org.openjdk.jmh:jmh-core:1.37")
     testAnnotationProcessor ("org.openjdk.jmh:jmh-generator-annprocess:1.37")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
 tasks.getByName<Test>("test") {
